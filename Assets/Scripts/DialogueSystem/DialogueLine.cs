e7fae099622929932df93fa7335769a262e11501
N0m0newman <39068297+N0m0newman@users.noreply.github.com>
2023-08-22 23:16:37 +0100

    Added ability for oslo to squeek

--- Assets/Scripts/DialogueSystem/DialogueLine.cs
+++ Assets/Scripts/DialogueSystem/DialogueLine.cs
@@ -6,6 +6,7 @@ public class DialogueLine
     public string line;
     public Sprite picture;
     public float revealTime;
+    public AudioSource audioSource;
 
     public DialogueLine(string name, string line, Sprite picture, float revealTime)
     {
@@ -13,5 +14,32 @@ public class DialogueLine
         this.line = line;
         this.picture = picture;
         this.revealTime = revealTime;
+        if(name.ToLower() == "oslo") ConfigureAudioSource();
     }
+
+    void ConfigureAudioSource()
+    {
+        switch(line.ToLower())
+        {
+            case "(happy squeak)":
+                break;
+            case "(curious squeak)":
+                break;
+            case "(injured squeak)":
+                break;
+            case "(confused squeak)":
+                break;
+            case "(excited squeak)":
+                break;
+            case "(alarmed squeak)":
+                break;
+            case "(sad squeak)":
+                break;
+            case "(angry squeak)":
+                break;
+            case "(determined squeak)":
+                break;
+        }
+    }
+
 }
